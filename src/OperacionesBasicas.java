bdf704f678688b9844226b388a787a96a8edd65f
Borja G.B <bgarciabarrera@danielcastelao.org>
2023-02-24 09:29:37 +0100

    Corregidos varios errores.

--- src/OperacionesBasicas.java
+++ src/OperacionesBasicas.java
@@ -21,7 +21,6 @@ public class OperacionesBasicas {
     /**
      * Elección de operación DIVIDIR
      *      *  static final int DIVIDIR = 1;
-     *
      */
     static final int DIVISION = 4;
 
